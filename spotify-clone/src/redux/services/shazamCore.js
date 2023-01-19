9f1692cfd651ba3437f5e485bec43977a351c0ea
Theologos Mavropoulos <rusinner88@gmail.com>
2023-01-19 16:08:50 +0200

    deleted comments

--- spotify-clone/src/redux/services/shazamCore.js
+++ spotify-clone/src/redux/services/shazamCore.js
@@ -1,16 +1,4 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-// const options = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "2db4e1eb6emsh40766a5abe17b10p18a780jsne7f8e982187e",
-//     "X-RapidAPI-Host": "shazam-core.p.rapidapi.com",
-//   },
-// };
-
-// fetch("https://shazam-core.p.rapidapi.com/v1/charts/world", options)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
 
 // the above code generated from api extension and i used everything below in redux toolkiit functions and objects
 
